48d204e2353097418574441932bee3e4c0f8bfc1
Aditya Ghatiya <91601167+Adityaghatiya@users.noreply.github.com>
2023-04-01 01:12:03 +0530

    Create Missing Number.java

---
+++ Missing Number/Missing Number.java
@@ -0,0 +1,14 @@
+class Solution {
+     public static int missingNumber(int arr[])
+    {
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]!=i)
+            {
+                return i;
+            }
+        }
+        return arr.length;
+    }
+}
